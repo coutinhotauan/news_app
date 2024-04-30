f9d1f79de756e1e07059b6f96bccbc2a16682c10
coutinhotauan <tauan_coutinho1@hotmail.com>
2024-04-30 15:36:47 -0300

    feat: start developing unit tests for home

--- test/features/home/data/datasource/home_datasource_test.dart
+++ test/features/home/data/datasource/home_datasource_test.dart
@@ -1,63 +1,51 @@
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:news_app/core/error/exception.dart';
-import 'package:news_app/features/search_news/data/datasources/news_search_datasource.dart';
+import 'package:news_app/features/home/data/datasource/home_datasource.dart';
 import 'package:news_app/features/search_news/data/models/news_search_model.dart';
 import 'package:pretty_http_logger/pretty_http_logger.dart';
-
-import '../../../../fixtures/fixture_reader.dart';
 import 'package:http/http.dart' as http;
 
-import 'news_search_datasource_test.mocks.dart';
+import '../../../../fixtures/fixture_reader.dart';
+import '../../../search_news/data/datasources/news_search_datasource_test.mocks.dart';
 
 @GenerateMocks([HttpWithMiddleware])
 void main() {
-  late NewsSearchDataSourceImpl datasource;
+  late HomeDatasourceImpl datasource;
   late MockHttpWithMiddleware client;
 
   setUp(
     () {
       client = MockHttpWithMiddleware();
-      datasource = NewsSearchDataSourceImpl(client: client);
+      datasource = HomeDatasourceImpl(client: client);
     },
   );
 
-  const tKeyword = "keyword";
-
-  const tUrl = "url";
-
-  final tNewsSearch =
-      NewsSearchModel.fromJson(json.decode(fixture("news_search.json")));
-
-  Map<String, dynamic> parameters = {
-    "apiKey": "key",
-    "q": "keyword",
-  };
+  final tNewsSearch = NewsSearchModel.fromJson(json.decode(fixture("news_search.json")));
 
   test(
-    'should return News Search when the status code is 200',
+    'should return a News Search when the statusCode is equal to 200',
     () async {
       when(client.get(any, headers: anyNamed("headers"))).thenAnswer(
         (_) async => http.Response(
           fixture("news_search.json"),
           200,
-          headers: {
-            HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
-          },
+          headers: {HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'},
         ),
       );
 
-      final result = await datasource.getNews(tKeyword);
+      final result = await datasource.getHeadlines();
 
       expect(result, equals(tNewsSearch));
     },
   );
 
   test(
-    'should throw a ServerException when the response code is 404 or other',
+    'should return a ServerException when statusCode is different than 200',
     () async {
       when(client.get(any, headers: anyNamed("headers"))).thenAnswer(
         (_) async => http.Response(
@@ -65,10 +53,10 @@ void main() {
           404,
         ),
       );
-      
-      final result = datasource.getNews;
 
-      expect(() => result(tKeyword), throwsA(const TypeMatcher<ServerException>()));
+      final result = datasource.getHeadlines();
+
+      expect(() => result, throwsA(const TypeMatcher<ServerException>()));
     },
   );
 }
